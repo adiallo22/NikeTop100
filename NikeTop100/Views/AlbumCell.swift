38b2c8e73738778cf7ac3e92aaf6f7da8f158d69
abdul diallo <rahimi.diallo96@gmail.com>
2020-08-28 11:13:23 -0400

    make the api calls on feed. use ViewModel to return album data in every cell.

---
+++ NikeTop100/Views/AlbumCell.swift
@@ -0,0 +1,75 @@
+//
+//  AlbumCell.swift
+//  NikeTop100
+//
+//  Created by Abdul Diallo on 8/28/20.
+//  Copyright © 2020 Abdul Diallo. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCell : UITableViewCell {
+    
+    var album : Album? {
+        didSet {
+            configAlbum()
+        }
+    }
+    
+    private var name : UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    private var thumbnail : UIImageView = {
+        let imgV = UIImageView()
+        return imgV
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+//MARK: - helpers
+
+extension AlbumCell {
+    
+    fileprivate func configUI() {
+        addSubview(thumbnail)
+        thumbnail.anchor(top: topAnchor, left: leftAnchor,
+                         bottom: bottomAnchor, right: rightAnchor,
+                         paddingTop: 5,
+                         paddingLeft: 5,
+                         paddingBottom: 5,
+                         paddingRight: 5)
+        addSubview(name)
+        name.anchor(left: leftAnchor,
+                    bottom: bottomAnchor,
+                    right: rightAnchor,
+                    paddingLeft: 16,
+                    paddingBottom: 16,
+                    paddingRight: 16)
+    }
+    
+    fileprivate func configAlbum() {
+        guard let album = album else { return }
+        let viewModel = AlbumViewModel.init(album: album)
+        name.text = "\(viewModel.name) by \(viewModel.artist)"
+        guard let url = viewModel.thumbnail else { return }
+        NetworkService.shared.downloadImage(withURL: url) { [weak self] image in
+            self?.thumbnail.image = image
+        }
+    }
+    
+}
+
